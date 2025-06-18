83dd52a4225d89400ac139b32f2a11137a86fb4c
juliarozenberg <jha.rozenberg@gmail.com>
2025-06-18 11:24:27 +0200

    dimensional stacking v1

---
+++ final assignment/open_exploration_dimensional_stacking.py
@@ -0,0 +1,88 @@
+# Dimensional stacking / scenario discovery
+
+# Import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+# Import the model and necessary functions from EMA Workbench
+from ema_workbench import Model, MultiprocessingEvaluator, Policy, Scenario
+
+from ema_workbench.em_framework.evaluators import perform_experiments
+from ema_workbench.em_framework.samplers import sample_uncertainties
+from ema_workbench.util import ema_logging
+import time
+from problem_formulation import get_model_for_problem_formulation
+
+
+if __name__ == "__main__":
+    ema_logging.log_to_stderr(ema_logging.INFO)
+
+    dike_model, planning_steps = get_model_for_problem_formulation(3)
+
+    # Build a user-defined scenario and policy:
+    reference_values = {
+        "Bmax": 175,
+        "Brate": 1.5,
+        "pfail": 0.5,
+        "ID flood wave shape": 4,
+        "planning steps": 2,
+    }
+    reference_values.update({f"discount rate {n}": 3.5 for n in planning_steps})
+    scen1 = {}
+
+    for key in dike_model.uncertainties:
+        name_split = key.name.split("_")
+
+        if len(name_split) == 1:
+            scen1.update({key.name: reference_values[key.name]})
+
+        else:
+            scen1.update({key.name: reference_values[name_split[1]]})
+
+    ref_scenario = Scenario("reference", **scen1)
+
+    # no dike increase, no warning, none of the rfr
+    zero_policy = {"DaysToThreat": 0}
+    zero_policy.update({f"DikeIncrease {n}": 0 for n in planning_steps})
+    zero_policy.update({f"RfR {n}": 0 for n in planning_steps})
+    pol0 = {}
+
+    for key in dike_model.levers:
+        s1, s2 = key.name.split("_")
+        pol0.update({key.name: zero_policy[s2]})
+
+    policy0 = Policy("Policy 0", **pol0)
+
+    # Call random scenarios or policies:
+    #    n_scenarios = 5
+    #    scenarios = sample_uncertainties(dike_model, 50)
+    #    n_policies = 10
+
+    # single run
+    #    start = time.time()
+    #    dike_model.run_model(ref_scenario, policy0)
+    #    end = time.time()
+    #    print(end - start)
+    #    results = dike_model.outcomes_output
+
+    # series run
+    #experiments, outcomes = perform_experiments(dike_model, ref_scenario, 5)
+
+    #print("Experiments:", experiments)
+
+# multiprocessing
+
+ema_logging.log_to_stderr(ema_logging.INFO)
+
+n_scenarios = 1000
+n_policies = 10
+
+with MultiprocessingEvaluator(dike_model, n_processes=-1) as evaluator:
+    results = evaluator.perform_experiments(n_scenarios, n_policies, 
+                                            uncertainty_sampling='sobol')
+
+
+
